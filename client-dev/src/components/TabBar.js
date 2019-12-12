aee7717dbd31d427d025370167065fd2cd3e8bbf
Yuri Braga <yuri@Yuri-PC.localdomain>
2019-12-11 23:56:31 -0300

    Eye candy added to tab bar

--- client-dev/src/components/TabBar.js
+++ client-dev/src/components/TabBar.js
@@ -3,12 +3,14 @@ import { Nav, NavItem, NavLink } from "reactstrap";
 
 class TabBar extends Component {
   state = {
-    lastEvent: null
+    lastEvent: null,
+    activeTab: "admin"
   };
 
   changeUrl = e => {
     e.preventDefault();
 
+    this.setState({ activeTab: e.target.name });
     this.props.history.push("/" + e.target.name);
   };
 
@@ -17,13 +19,23 @@ class TabBar extends Component {
       <div>
         <Nav tabs>
           <NavItem>
-            <NavLink href="/admin" name="admin" onClick={this.changeUrl}>
+            <NavLink
+              href="/admin"
+              name="admin"
+              onClick={this.changeUrl}
+              active={this.state.activeTab === "admin" ? true : false}
+            >
               Administração
             </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/sensores" name="sensores" onClick={this.changeUrl}>
-              Sensores
+            <NavLink
+              href="/sensores"
+              name="sensores"
+              onClick={this.changeUrl}
+              active={this.state.activeTab === "sensores" ? true : false}
+            >
+              Relatórios
             </NavLink>
           </NavItem>
         </Nav>
