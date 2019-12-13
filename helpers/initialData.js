e290a245d31e467bac0a7b518eef72aead0938b0
Yuri Braga <yuri@Yuri-PC.localdomain>
2019-12-13 15:26:12 -0300

    Missed tamanho and localizacao tables

--- helpers/initialData.js
+++ helpers/initialData.js
@@ -2,6 +2,8 @@ const Marca = require("../db/models").Marca;
 const Tipo = require("../db/models").Tipo;
 const Tensao = require("../db/models").Tensao;
 const Sensor = require("../db/models").Sensor;
+const Tamanho = require("../db/models").Tamanho;
+const Localizacao = require("../db/models").Localizacao;
 
 const addData = async function() {
   let marcasIniciais = [
@@ -50,20 +52,33 @@ const addData = async function() {
 
   let sensorInicial = {
     codename: "Sensor teste 1",
-    latitude: -19.8157,
-    longitude: -43.9542,
-    altura: 2,
-    largura: 2,
-    comprimento: 2,
     tensao_id: 2,
     tipo_id: 1,
     marca_id: 1
   };
 
+  let tamanhoInicial = {
+    altura: 2,
+    largura: 2,
+    comprimento: 2
+  };
+
+  let localizacaoInicial = {
+    latitude: -19.8157,
+    longitude: -43.9542
+  };
+
   await Marca.bulkCreate(marcasIniciais);
   await Tipo.bulkCreate(tiposIniciais);
   await Tensao.bulkCreate(tensoesIniciais);
-  await Sensor.create(sensorInicial);
+
+  Sensor.create(sensorInicial).then(async sensor => {
+    tamanhoInicial.sensor_id = sensor.id;
+    localizacaoInicial.sensor_id = sensor.id;
+
+    await Tamanho.create(tamanhoInicial);
+    await Localizacao.create(localizacaoInicial);
+  });
 };
 
 module.exports = addData;
